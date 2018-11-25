52686f0f1c7319dc53a0b5b098ab5f82c354d48b
Thales Fukuda Celestino <thalesfukudacelestino@Thaless-MacBook-Pro.local>
2018-11-25 01:20:03 -0200

    Add images and list of images

---
+++ src/movies.js
@@ -0,0 +1,26 @@
+const PATH = './assets/images/movies';
+
+const movies = [
+    { image: `${PATH}/castle_in_the_sky.jpg` },
+    { image: `${PATH}/grave_of_the_fireflies.png` },
+    { image: `${PATH}/totoro.jpg` },
+    { image: `${PATH}/kiki.jpg` },
+    { image: `${PATH}/only_yesterday.jpg` },
+    { image: `${PATH}/porco_rosso.jpg` },
+    { image: `${PATH}/pom_poko.jpeg` },
+    { image: `${PATH}/whisper_of_the_heart.jpg` },
+    { image: `${PATH}/princess_mononoke.jpg` },
+    { image: `${PATH}/yamadas.jpg` },
+    { image: `${PATH}/spirited_away.jpg` },
+    { image: `${PATH}/cat_returns.jpg` },
+    { image: `${PATH}/moving_castle.jpg` },
+    { image: `${PATH}/tales_earthsea.webp` },
+    { image: `${PATH}/ponyo.jpg` },
+    { image: `${PATH}/arrietty.jpg` },
+    { image: `${PATH}/poppy_hill.jpg` },
+    { image: `${PATH}/the_wind_rises.jpg` },
+    { image: `${PATH}/princess_kaguya.jpg` },
+    { image: `${PATH}/marnie.jpg` },
+]
+
+export default movies;
